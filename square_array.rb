a9b7df232114fc7cba61d75c6637bb8b31d3d41e
Emma Konrad <emma.konrad92@gmail.com>
2019-01-30 04:27:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
 def square_array(numbers)
+  new_array = []
   numbers.each do |number|
-    new_array = []
-    new_array.push(square)
+    new_array << numbere ** 2 
+    return new_array
   end
 end
